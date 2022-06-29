6d298d99d8f5fc89f3780a2b3cb86cb55d51f9da
Davlat <dushurbakiev@gmail.com>
2022-06-29 11:02:09 +0600

    added db structure, models and migrations

---
+++ database/migrations/2022_06_29_044549_create_posts_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up(): void
+    {
+        Schema::create('posts', static function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->longText('content');
+            $table->foreignId('category_id')->nullable()->references('id')->on('categories');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('posts');
+    }
+};
