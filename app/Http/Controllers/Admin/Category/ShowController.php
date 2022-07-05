e2150a7158512606ef48312f3b7db84e35d733ed
Davlat <dushurbakiev@gmail.com>
2022-07-05 18:08:03 +0600

    added category show

---
+++ app/Http/Controllers/Admin/Category/ShowController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers\Admin\Category;
+
+use App\Http\Controllers\Controller;
+use App\Models\Category;
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Contracts\View\Factory;
+use Illuminate\Contracts\View\View;
+use Illuminate\Http\Request;
+
+class ShowController extends Controller
+{
+    public function __invoke(Category $category): Factory|View|Application
+    {
+        return view('admin.category.show',compact('category'));
+    }
+}
--- resources/views/admin/category/show.blade.php
+++ resources/views/admin/category/show.blade.php
@@ -7,12 +7,12 @@
             <div class="container-fluid">
                 <div class="row mb-2">
                     <div class="col-sm-6">
-                        <h1 class="m-0">Categories</h1>
+                        <h1 class="m-0">Category</h1>
                     </div><!-- /.col -->
                     <div class="col-sm-6">
                         <ol class="breadcrumb float-sm-right">
                             <li class="breadcrumb-item"><a href="{{route('admin.main.index')}}">Home</a></li>
-                            <li class="breadcrumb-item active">Index categories</li>
+                            <li class="breadcrumb-item active">Category info</li>
                         </ol>
                     </div><!-- /.col -->
                 </div><!-- /.row -->
@@ -23,41 +23,26 @@
         <section class="content">
             <div class="container-fluid">
                 <!-- Small boxes (Stat box) -->
-                <div class="row">
-                    <div class="col-12">
-                        <a href="{{route('admin.category.create')}}">
-                            <button class="btn btn-primary">
-                                Add new category
-                            </button>
-                        </a>
-                    </div>
-                </div>
                 <div class="row pt-4">
                     <div class="col-8">
                         <div class="card">
                             <div class="card-header">
-                                <h3 class="card-title">List of categories</h3>
+                                <h3 class="card-title">Category information</h3>
                             </div>
                             <!-- /.card-header -->
                             <div class="card-body table-responsive p-0">
                                 <table class="table table-hover text-nowrap">
-                                    <thead>
+                                    <tbody>
                                     <tr>
                                         <th>ID</th>
                                         <th>Title</th>
-                                        <th></th>
                                         <th>Created at</th>
                                     </tr>
-                                    </thead>
-                                    <tbody>
-                                    @foreach($categories as $category)
                                     <tr>
                                         <td>{{$category->id}}</td>
                                         <td>{{$category->title}}</td>
-                                        <td><a href="{{route('admin.category.show',$category->id)}}"><i class="fa fa-eye"></i></a></td>
                                         <td>{{$category->created_at}}</td>
                                     </tr>
-                                    @endforeach
                                     </tbody>
                                 </table>
                             </div>
